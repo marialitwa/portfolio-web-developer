a6b5015c728798cba35a039841c0734104aa066f
Maria Litwa <contact@marialitwa.com>
2024-03-20 15:24:06 +0100

    Add links with animations to intro section.

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,10 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -49,18 +53,80 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-
       <motion.p
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-2xl font-medium 
+        !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hej, I'm Maria.</span> I'm a{" "}
         <span className="font-bold">frontend web developer</span> with a{" "}
-        <span className="font-bold">background in design</span> and photography.
+        background in <span className="font-bold">design</span> and photography.
         I enjoy building <span className="italic">sites & apps</span>. My focus
         is <span className="underline">React (Next.js)</span>.
       </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row 
+        items-center justify-center gap-4 px-4 text-lg"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 
+          flex items-center gap-2 rounded-full outline-none 
+          focus:scale-110 hover:scale-110 hover:bg-gray-950 
+          active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight
+            className="opacity-70 
+          group-hover:translate-x-1 transition"
+          />{" "}
+        </Link>
+
+        <a
+          href="/assets/CV_BlankoTest.pdf"
+          download={true}
+          className="group bg-white px-7 py-3 flex items-center 
+          gap-2 rounded-full font-medium outline-none focus:scale-110 
+          hover:scale-110 active:scale-105 transition cursor-pointer 
+          border border-black/10"
+        >
+          Download CV{" "}
+          <HiDownload
+            className="opacity-60 
+          group-hover:translate-y-1 transition"
+          />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/maria-litwa/"
+          target="_blank"
+          className="bg-white p-4 text-gray-800 flex 
+          items-center gap-2 rounded-full focus:scale-[1.15] 
+          hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition 
+          cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://www.github.com/marialitwa"
+          target="_blank"
+          className="bg-white p-4 text-gray-800 flex 
+          items-center gap-2 rounded-full text-[1.35rem] 
+          focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 
+          active:scale-105 transition 
+          cursor-pointer border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
