bac425c4bf6e33e6b08ffff1587b53ef48e7cfa2
Maria Litwa <contact@marialitwa.com>
2024-03-25 14:14:42 +0100

    Add animation to skills section.

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,6 +4,22 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  // creates a staggered animation for each index item having a delay of 0.05s more than the previous one
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 export default function Skills() {
   const { ref } = useSectionInView("Skills");
@@ -14,14 +30,27 @@ export default function Skills() {
       id="skills"
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
       // scroll-mt-28 => will scroll to section and start of section will have a margin-top of 28
-      // otherwise the section container start will be at the viewport top pixel
+      // otherwise the section container start will be at the viewport top pixel 0
     >
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li className="bg-white borderBlack rounded-xl px-5 py-3" key={index}>
+          <motion.li
+            className="bg-white borderBlack rounded-xl px-5 py-3"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            // Below: whileInView is a Framer Motion thing => animation happens only while in viewport
+            whileInView="animate"
+            // Below: Animation will only happen ONCE while in viewport
+            viewport={{
+              once: true,
+            }}
+            // Below: custom is need for staggering effect => check fadeInAnimationVariants variable
+            custom={index}
+          >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
